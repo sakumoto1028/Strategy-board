6ec324c59d71aed1bfae7bf97b8a47275c34b41e
kazuaki.sakumoto <sakumoto1028@icloud.com>
2021-01-25 19:52:36 +0900

    Initial Commit

---
+++ Strategy board/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strategy board
+//
+//  Created by 作本和陽 on 2021/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
